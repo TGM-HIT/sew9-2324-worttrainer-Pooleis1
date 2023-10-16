eb7f6af56115ca3e9c12227ceaf945f481f2a980
Pia <94532135+Pooleis1@users.noreply.github.com>
2023-10-16 07:32:26 +0200

    mit dem COde begonnen

---
+++ src/main/java/tgm/ac/at/parnold/JSONSaveManager.java
@@ -0,0 +1,15 @@
+package tgm.ac.at.parnold;
+
+public class JSONSaveManager implements WordTrainerSaveManager{
+
+
+    @Override
+    public void save(String filepath, WordspellTrainer wordTrainer) {
+
+    }
+
+    @Override
+    public WordspellTrainer load(String filepath) {
+        return null;
+    }
+}
